a2600a5761b2b94560f5905f96dfd6dd4296de52
muhammed rameez <31467986+mhmdrameez@users.noreply.github.com>
2023-02-27 17:14:35 +0530

    updated

--- backend/config/db.js
+++ backend/config/db.js
@@ -2,7 +2,7 @@ import mongoose from 'mongoose'
 
 const connectDB = async () => {
   try {
-    const conn = await mongoose.connect("mongodb://localhost:27017", {
+    const conn = await mongoose.connect("mongodb+srv://buildx1234:buildx1234@build-x.f4hzn.mongodb.net/buildx?retryWrites=true&w=majority", {
       useUnifiedTopology: true,
       useNewUrlParser: true,
       useCreateIndex: true,
